7c0b58476b92484f34877c953e623c48157f1ca5
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-02-15 13:56:28 -0700

    trim leading and trailing white space from links figured out correct order and regex to get the article text

--- 00_article_download.R
+++ 00_article_download.R
@@ -19,20 +19,12 @@ id <- gdrive_files[gdrive_files$name == "New Article Coding Framework", ]$id
 drive_download(id, path = "data/new_article_coding.csv", overwrite = TRUE)
 
 article_codes <- read.csv(file = 'data/new_article_coding.csv')
-folder_url <- "https://drive.google.com/drive/u/0/folders/1ob5sagTtT3svhc7ZKeemd9TiAq1_MsCL"
-folder <- drive_get(as_id(folder_url))
-
-gdrive_files <- drive_ls(folder)
-id <- gdrive_files[gdrive_files$name == "Article Coding", ]$id
-drive_download(id, path = "data/article_coding.csv", overwrite = TRUE)
-
-article_codes <- read.csv(file = 'data/article_coding.csv')
 
 article_codes <- article_codes %>% 
   filter(!Link=='') %>% 
   mutate(id = row_number())
 
-urls <- unique(article_codes$Link)
+urls <- trimws(unique(article_codes$Link))
 
 # set up a user agent so that the website doesn't think you are a robot
 ua <- "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
@@ -43,16 +35,31 @@ page_text <- httr::content(page, as = "text")
 text_body <- str_extract_all(page_text, regex("(?<=<p>).+(?=</p>)", multiline = TRUE), simplify = TRUE) 
 text <- text_body[[2]]
 
+test <- gsub(".*h1", "", page_text) 
+test2 <- gsub("(?<=")
+
 str_extract_all(page_text, regex("(?<=<h1).+(?=</h1>)"), simplify = TRUE) 
 str_extract(page_text, regex("(?<=<h1).*?(?=</h1>)")) 
 
 str_extract_all(page_text, regex("(?<=<p>).+"), simplify = TRUE)
-test <- str_extract_all(page_text, regex("(?<=<p>).+", multiline = TRUE), simplify = TRUE) # only goes up to new line break of paragraph that I want.
+#test <- str_extract_all(page_text, regex("(?<=<p>).+", multiline = TRUE), simplify = TRUE) # only goes up to new line break of paragraph that I want.
 str_count(page_text, "<p>")
 
 page_text_noline <- str_remove_all(page_text, "\n")
+test_noline <- str_remove_all(test, "\n")
+
+str_extract(test_noline, regex("(?<=<p>).+?(?=</p>)")) ## this worked!!!
+str_extract_all(test_noline, regex(".*(?=</p>)"), simplify = TRUE) 
 
 str_extract_all(page_text_noline, regex("(?<=<p>).+"), simplify = TRUE) 
+
+#########
+link <- urls[1]
+page <- GET(link, user_agent(ua))
+page_text <- httr::content(page, as = "text")
+test <- gsub(".*h1", "", page_text) 
+test_noline <- str_remove_all(test, "\n")
+str_extract(test_noline, regex("(?<=<p>).+?(?=</p>)")) ## this worked!!!
 
 ## Breadcrumb for next time is to figure out the regular expression.
 
