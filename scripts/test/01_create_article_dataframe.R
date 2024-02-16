31c71dafb2c6464f8994edb31e0f854314664bcb
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-02-16 16:21:16 -0700

    code moved from 00_download_data.R script Need to figure out loop and skipping articles we already have so that we don't continuously download the same 200 articles

---
+++ scripts/test/01_create_article_dataframe.R
@@ -0,0 +1,41 @@
+library(httr)
+library(tidyverse)
+library(stringr)
+library(rvest)
+library(googledrive)
+options(
+  gargle_oauth_cache = ".secrets",
+  gargle_oauth_email = TRUE
+)
+
+# Load the new_article_coding_ids.csv
+article_codes <- read.csv(file = "data/processed/new_article_coding_ids.csv")
+
+# Create list of article urls
+## need to trim the leading and trailing white spaces
+urls <- trimws(unique(article_codes$Link))
+
+# Set up a user agent so that the website doesn't think you are a robot
+ua <- "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+
+# Create an empty date. We will append the new article to this data frame
+df_article <- data.frame(Title=character(),
+                         Author=character(),
+                         Published_Date=character(),
+                         Article_Text=character())
+
+for (url in urls[1:2]){
+  link <- url
+  page <- GET(link, user_agent(ua))
+  page_text <- httr::content(page, as = "text")
+  tmp <- gsub(".*<h1", "", page_text) 
+  article_title <- str_extract_all(tmp, regex("(?<=clipcopy\">).+?(?=</h1>)"))
+  author <- str_extract_all(tmp, regex("(?<=Byline: ).+?(?=</span>)"))
+  pub_date <- str_extract_all(tmp, regex("(?<=display-date\">).+?(?=</span>)"))
+  tmp_noline <- str_remove_all(tmp, "\n")
+  article_text <- str_extract(tmp_noline, regex("(?<=<p>).+?(?=</p>)"))
+  df_article[nrow(df_article) + 1,] <- c(article_title, 
+                                         author, 
+                                         pub_date, 
+                                         article_text)
+}
