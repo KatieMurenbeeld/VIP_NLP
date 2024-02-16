ecba646f5041903857a8d4de2370f04ccb0ce2b8
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-02-16 15:07:53 -0700

    made into for loop, added empty dataframe to fill

--- 00_article_download.R
+++ 00_article_download.R
@@ -29,59 +29,25 @@ urls <- trimws(unique(article_codes$Link))
 # set up a user agent so that the website doesn't think you are a robot
 ua <- "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
 
-link <- urls[1]
-page <- GET(link, user_agent(ua))
-page_text <- httr::content(page, as = "text")
-tmp <- gsub(".*<h1", "", page_text) 
-article_title <- str_extract_all(tmp, regex("(?<=clipcopy\">).+?(?=</h1>)"))
-author <- str_extract_all(tmp, regex("(?<=Byline: ).+?(?=</span>)"))
-pub_date <- str_extract_all(tmp, regex("(?<=display-date\">).+?(?=</span>)"))
-tmp_noline <- str_remove_all(tmp, "\n")
-article_text <- str_extract(tmp_noline, regex("(?<=<p>).+?(?=</p>)"))
+df_article <- data.frame(Title=character(),
+                         Author=character(),
+                         Published_Date=character(),
+                         Article_Text=character())
 
-test <- gsub(".*h1", "", page_text) 
-test2 <- gsub("(?<=")
+for (url in urls[1:2]){
+  link <- url
+  page <- GET(link, user_agent(ua))
+  page_text <- httr::content(page, as = "text")
+  tmp <- gsub(".*<h1", "", page_text) 
+  article_title <- str_extract_all(tmp, regex("(?<=clipcopy\">).+?(?=</h1>)"))
+  author <- str_extract_all(tmp, regex("(?<=Byline: ).+?(?=</span>)"))
+  pub_date <- str_extract_all(tmp, regex("(?<=display-date\">).+?(?=</span>)"))
+  tmp_noline <- str_remove_all(tmp, "\n")
+  article_text <- str_extract(tmp_noline, regex("(?<=<p>).+?(?=</p>)"))
+  df_article<- rbind(df_article, c(article_title, author, pub_date, article_text))
+}
 
-str_extract_all(page_text, regex("(?<=<h1).+(?=</h1>)"), simplify = TRUE) 
-str_extract(page_text, regex("(?<=<h1).*?(?=</h1>)")) 
-
-str_extract_all(page_text, regex("(?<=<p>).+"), simplify = TRUE)
-#test <- str_extract_all(page_text, regex("(?<=<p>).+", multiline = TRUE), simplify = TRUE) # only goes up to new line break of paragraph that I want.
-str_count(page_text, "<p>")
-
-page_text_noline <- str_remove_all(page_text, "\n")
-test_noline <- str_remove_all(test, "\n")
-
-str_extract(test_noline, regex("(?<=<p>).+?(?=</p>)")) ## this worked!!!
-str_extract_all(test_noline, regex(".*(?=</p>)"), simplify = TRUE) 
-
-str_extract_all(page_text_noline, regex("(?<=<p>).+"), simplify = TRUE) 
-
-#########
-link <- urls[1]
-page <- GET(link, user_agent(ua))
-page_text <- httr::content(page, as = "text")
-test <- gsub(".*h1", "", page_text) 
-test_noline <- str_remove_all(test, "\n")
-str_extract(test_noline, regex("(?<=<p>).+?(?=</p>)")) ## this worked!!!
 
 ## Breadcrumb for next time is to finish updating this code up to creating the data frame combining the new article code framework with the article text.
 
-##############################
-# Testing out with rvest
-page_html <- read_html(link, user_agent = ua)
-  
-# full Xpath to article content
-# Title
-# /html/body/div[2]/div[3]/section[2]/main/div[2]/div/div[1]/h1
-# Newspaper info
-# /html/body/div[2]/div[3]/section[2]/main/div[2]/div/div[1]/div[1]/div[1]
-# Author byline
-# /html/body/div[2]/div[3]/section[2]/main/div[2]/div/div[1]/div[1]/div[2]
-# Article text
-# /html/body/div[2]/div[3]/section[2]/main/div[2]/div/div[1]/div[3]/p
-# //*[@id="document-view--ascii"]/div/div[1]/div[3]/p
-test_p <- page_html %>%
-  html_nodes(xpath = '//*[@id="document-view--ascii"]/div/div[1]/div[3]/p') %>%
-  html_text2()
 
