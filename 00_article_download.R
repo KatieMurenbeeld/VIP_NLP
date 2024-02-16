d119096560b0c6776d437cad2db890cb4d747e16
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-02-16 16:19:44 -0700

    moved code to  01_create_article_dataframe.R script

--- 00_article_download.R
+++ 00_article_download.R
@@ -16,41 +16,17 @@ folder <- drive_get(as_id(folder_url))
 
 gdrive_files <- drive_ls(folder)
 id <- gdrive_files[gdrive_files$name == "New Article Coding Framework", ]$id
-drive_download(id, path = "data/new_article_coding.csv", overwrite = TRUE)
+drive_download(id, path = "data/original/new_article_coding.csv", overwrite = TRUE)
 
-article_codes <- read.csv(file = 'data/new_article_coding.csv')
+article_codes <- read.csv(file = "data/original/new_article_coding.csv")
 
+# Create a unique article ID
 article_codes <- article_codes %>% 
-  filter(!Link=='') %>% 
-  mutate(id = row_number())
-
-urls <- trimws(unique(article_codes$Link))
-
-# set up a user agent so that the website doesn't think you are a robot
-ua <- "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-
-df_article <- data.frame(Title=character(),
-                         Author=character(),
-                         Published_Date=character(),
-                         Article_Text=character())
-
-for (url in urls[1:2]){
-  link <- url
-  page <- GET(link, user_agent(ua))
-  page_text <- httr::content(page, as = "text")
-  tmp <- gsub(".*<h1", "", page_text) 
-  article_title <- str_extract_all(tmp, regex("(?<=clipcopy\">).+?(?=</h1>)"))
-  author <- str_extract_all(tmp, regex("(?<=Byline: ).+?(?=</span>)"))
-  pub_date <- str_extract_all(tmp, regex("(?<=display-date\">).+?(?=</span>)"))
-  tmp_noline <- str_remove_all(tmp, "\n")
-  article_text <- str_extract(tmp_noline, regex("(?<=<p>).+?(?=</p>)"))
-  df_article[nrow(df_article) + 1,] <- c(article_title, 
-                                        author, 
-                                        pub_date, 
-                                        article_text)
-}
-
-
-## Breadcrumb for next time is to finish updating this code up to creating the data frame combining the new article code framework with the article text.
+  filter(!Link=='') %>%
+  mutate(tmp = match(Link, unique(Link)),
+         ID = sprintf('Article%003d', tmp)) %>%
+  select(-tmp)
+# Save as a new csv in the data/processed folder
+write.csv(article_codes, "data/processed/new_article_coding_ids.csv")
 
 
