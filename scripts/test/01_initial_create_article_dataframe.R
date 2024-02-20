7dba69ea94e6a59c5dbf2859b740ee3272ca39a5
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-02-20 16:39:40 -0700

    renamed script. added breadcrumb for tomorrow

--- scripts/test/01_initial_create_article_dataframe.R
+++ scripts/test/01_initial_create_article_dataframe.R
@@ -20,8 +20,9 @@ article_list_new <- article_codes %>%
 
 # Create list of article urls
 ## need to trim the leading and trailing white spaces
-article_links <- anti_join(article_list_new, article_list_prev, by="ID")
-urls <- trimws(unique(article_links$Link))
+#article_links <- anti_join(article_list_new, article_list_prev, by="ID")
+#urls <- trimws(unique(article_links$Link))
+urls <- trimws(unique(article_list_prev$Link))
 
 # Set up a user agent so that the website doesn't think you are a robot
 ua <- "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
@@ -30,9 +31,10 @@ ua <- "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like
 df_article <- data.frame(Title=character(),
                          Author=character(),
                          Published_Date=character(),
-                         Article_Text=character())
+                         Article_Text=character(), 
+                         Link=character())
 
-for (url in urls[1:2]){
+for (url in urls[3:5]){
   link <- url
   page <- GET(link, user_agent(ua))
   page_text <- httr::content(page, as = "text")
@@ -45,8 +47,31 @@ for (url in urls[1:2]){
   df_article[nrow(df_article) + 1,] <- c(article_title, 
                                          author, 
                                          pub_date, 
-                                         article_text)
+                                         article_text,
+                                         link)
 }
 
+## Not sure if this is necessary or if I can just join by the link
+#df_article$ID <- df_article %>% 
+#  filter(!Link=='') %>%
+#  mutate(tmp = match(Link, unique(Link)),
+#         ID = sprintf('Article%003d', tmp)) %>%
+#  select(-tmp)
+
+## Write df to csv
+write_csv(df_article, "data/processed/article_text.csv", col_names = TRUE, append = TRUE)
+
+
+## Join df_article to new_article_codes_id.csv
+## Trim whitespace from links
+article_codes$Link <- trimws(article_codes$Link)
+df_text_codes <- full_join(article_codes, df_article, by="Link")
+
+## Write joined df to csv
+write_csv(df_text_codes, "data/processed/article_codes_text.csv", )
+
 #################
-# Breadcrumb Need to figure out loop and skipping articles we already have so that we don't continuously download the same 200 articles
+# Breadcrumb: Need to figure out if else for articles with no by lines (if character(0) then NA else text)
+
+
+
