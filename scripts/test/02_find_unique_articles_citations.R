100fa1f2a71044255d0617f97633ddf6fb0d681b
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-05 15:51:30 -0700

    working with text data from proquest download

--- scripts/test/02_find_unique_articles_citations.R
+++ scripts/test/02_find_unique_articles_citations.R
@@ -4,3 +4,19 @@ library(stringr)
 pq_articles <- read_csv("data/processed/citation_29-02-2024.csv")
 
 articles <- as.data.frame(unique(pq_articles$Title))
+
+#---Load in the text files from ProQuest-----
+
+data <- read_file("data/original/ProQuestDocuments-2024-03-05.txt")
+
+sep <- "____________________________________________________________"
+
+pattern <- paste0(sep,"\\s*(.*?)\\s*",sep)
+
+result <- regmatches(data, gregexpr(pattern, data))
+
+df <- result %>%
+  map_df(as_tibble)
+
+
+                            
