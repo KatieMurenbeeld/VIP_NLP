03299b1ace8c1faf090ae9fcda0742fbb2d70391
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-06 13:52:39 -0700

    clean up code, clean up text data

--- scripts/test/02_find_unique_articles_citations.R
+++ scripts/test/02_find_unique_articles_citations.R
@@ -10,23 +10,6 @@ articles <- as.data.frame(unique(pq_articles$Title))
 data <- read_file("data/original/ProQuestDocuments-2024-03-05_02.txt")
 
 #Separate the articles  
-
-sep <- "____________________________________________________________"
-
-pattern <- paste0(sep,"\\s*(.*?)\\s*",sep)
-
-result <- regmatches(data, gregexpr(pattern, data))
-
-df <- result %>%
-  map_df(as_tibble)
-
-number <- 1:84
-doc_num <- paste0("Document ", number, " ")
-
-pattern_2 <- paste0(doc_num,"\\s*(.*?)\\s*",sep)
-
-
-
 new_df <- data.frame(doc_id=character(),
                      info=character())
 
@@ -37,28 +20,20 @@ doc_num <- paste0("Document ", number, " ")
 doc_id <- list()
 info <- list()
 
-for (i in doc_num[1:3]){
-  #print(i)
+for (i in doc_num){
   tmp_id <- i
-  print(tmp_id)
   tmp_pattern <- paste0(i,"\\s*(.*?)\\s*",sep)
-  print(tmp_pattern)
   tmp_info <- regmatches(data, gregexpr(tmp_pattern, data))
-  print(tmp_info)
-  #dat <- data.frame(x = info)
-  #doc_id <- append(doc_id, list(tmp_id))
-  #info <- append(info, list(tmp_info))
   doc_id <- tmp_id
   info <- tmp_info
   new_df[nrow(new_df) + 1,] <- c(doc_id, 
                                  info)
 }
 
-df_2 <- as.data.frame(output)
-result_2 <- regmatches(data, gregexpr(pattern_2, data))
+articles <- new_df %>%
+  separate_rows(info, sep = "\r\n\r\n")
 
-df_2 <- result_2 %>%
-  map_df(as_tibble)
+write.csv(articles, file = here::here("data/processed/02_module_articles.csv"))
 
 library(janeaustenr)
 
