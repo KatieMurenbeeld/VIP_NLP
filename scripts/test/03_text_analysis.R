a3e1e75002024cdea497ba94892f0be2205de2b5
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-06 14:44:50 -0700

    data frame with document id, article title, and article text

--- scripts/test/03_text_analysis.R
+++ scripts/test/03_text_analysis.R
@@ -15,3 +15,14 @@ original_books <- austen_books() %>%
 # read in the data
 
 original_articles <- read_csv(here::here("data/processed/02_module_articles_2024-03-06.csv"))
+
+articles_text <- original_articles %>%
+  group_by(doc_id) %>%
+  mutate(title = str_extract(text,
+                            regex("(?<=Title:).*")),
+         full_text = str_extract(text, 
+                                 regex("(?<=Full text:).[\\s\\S]*"))) %>%
+  fill(title, .direction = "downup") %>%
+  drop_na(full_text) %>%
+  select(-text) %>%
+  ungroup()
