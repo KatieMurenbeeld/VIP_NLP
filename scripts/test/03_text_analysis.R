96967e657108b9e8e152b9222d6f06881c532803
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-06 21:40:45 -0700

    added stemming library (snowballC) making figures filtering by species

--- scripts/test/03_text_analysis.R
+++ scripts/test/03_text_analysis.R
@@ -1,6 +1,7 @@
 library(stringr)
 library(tidyverse)
 library(tidytext)
+library(SnowballC)
 library(ggplot2)
 library(janeaustenr)
 
@@ -39,23 +40,49 @@ tdm_titles <- trimws(unique(articles_text$title))
 # Join the articles text to the coded data
 articles_joined <- left_join(articles_text, article_codes, by = c("title" = "Title"))
 
-tidy_articles <- articles_text %>%
+tidy_articles <- articles_joined %>%
   unnest_tokens(word, full_text)
 
 tidy_articles %>%
   count(word, sort = TRUE) 
 
 data("stop_words")
+# Create a small data frame of your own stop words for this project 
+# ("br" and "strong" are part of the html formatting which should come out before making tidy df!)
+# This needs to be a data frame so that you can use "anti-join()"
+# You can add your own words to this list
+my_stop_words <- data.frame(c("facebook", "twitter", "whatsapp", "print", "sms", "copy", "email", "http", "https")) 
+colnames(my_stop_words) <-("word")
+
+tidy_articles <- tidy_articles %>%
+  anti_join(stop_words) %>%
+  anti_join(my_stop_words) %>% 
+  mutate(stem = wordStem(word))
 
 tidy_articles %>%
-  anti_join(stop_words) %>%
   count(word, sort = TRUE) 
 
 tidy_articles %>%
-  anti_join(stop_words) %>%
   count(word, sort = TRUE) %>%
-  filter(n > 100) %>%
+  filter(n > 200) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(n, word)) +
   geom_col() +
   labs(y = NULL)
+
+unique(tidy_articles$Species)
+
+species <- c("Boars")
+
+tidy_articles %>%
+  filter(Species %in% species) %>%
+  count(word, sort = TRUE) %>%
+  slice_max(n, n=10) %>%
+  filter(n > 25) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word)) +
+  geom_col() +
+  labs(y = NULL) + 
+  ggtitle(paste(species, "10 most common words"))
+
+
