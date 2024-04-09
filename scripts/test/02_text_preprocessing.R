06ee890c8b0e291f3d701df5bff5dab359758eb3
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-04-09 16:26:10 -0600

    data cleaning

--- scripts/test/02_text_preprocessing.R
+++ scripts/test/02_text_preprocessing.R
@@ -6,39 +6,56 @@ library(ggplot2)
 library(tigris)
 
 # Read in the data
-text_codes <- read.csv(here::here("data/processed/article_text_codes_2024-04-04.csv"))
+article_codes <- read.csv(file = "data/original/new_article_coding.csv")
+
+text_grizz <- read.csv(here::here("data/processed/article_text_2024-04-04.csv"))
+text_coyote <- read.csv(here::here("data/processed/article_text_coyote_2024-04-08.csv"))
+text_boars <- read.csv(here::here("data/processed/article_text_boars_2024-04-08.csv"))
+text_wolf <- read.csv(here::here("data/processed/article_text_wolf_2024-04-08.csv"))
+text_beavs <- read.csv(here::here("data/processed/article_text_beavs_2024-04-08.csv"))
+
+text_all <- rbind(text_grizz, text_beavs, text_boars, text_coyote, text_wolf)
+
+## join to the article codes
+article_codes$Link <- trimws(article_codes$Link)
+text_all$Link <- trimws(text_all$Link)
+
+df_text_codes <- right_join(text_all, article_codes, by="Link")
+df_text_codes <- df_text_codes %>%
+  filter(grepl(" ", df_text_codes$Article_Text) == TRUE)
+df_text_codes <- df_text_codes %>%
+  filter(Species != "Bob Cats")
 
 # Preprocess the data
-articles_text <- text_codes %>%
-  filter(Species == "Grizzly Bear" | Species == "Grizzly Bears")
-
 ## Clean up multiple-spellings in species names, focus, and conflict type
-articles_text$Species <- str_replace(articles_text$Species, 'Grizzly Bears', 'Grizzly Bear')
-articles_text$Focus <- str_replace(articles_text$Focus, 'Practicioner', 'Practitioner')
-articles_text$Conflict_Type <- str_replace(articles_text$Conflict_Type, 'H-H', 'Human-Human')
-articles_text$Conflict_Type <- str_replace(articles_text$Conflict_Type, 'H-W', 'Human-Wildlife')
-articles_text$Conflict_Type <- str_replace(articles_text$Conflict_Type, 'N-W', 'Nature-Wildlife')
-articles_text$Conflict_Type <- str_replace(articles_text$Conflict_Type, 'Unstated Conflict', 'Unstated')
+df_text_codes$Species <- str_replace(df_text_codes$Species, 'Grizzly Bears', 'Grizzly Bear')
+df_text_codes$Focus <- str_replace(df_text_codes$Focus, 'Practicioner', 'Practitioner')
+df_text_codes$Focus <- str_replace(df_text_codes$Focus, 'Practictioner', 'Practitioner')
+df_text_codes$Focus <- str_replace(df_text_codes$Focus, 'Wildllife', 'Wildlife')
+df_text_codes$Conflict_Type <- str_replace(df_text_codes$Conflict_Type, 'H-H', 'Human-Human')
+df_text_codes$Conflict_Type <- str_replace(df_text_codes$Conflict_Type, 'H-W', 'Human-Wildlife')
+df_text_codes$Conflict_Type <- str_replace(df_text_codes$Conflict_Type, 'N-W', 'Nature-Wildlife')
+df_text_codes$Conflict_Type <- str_replace(df_text_codes$Conflict_Type, 'Unstated Conflict', 'Unstated')
 
 
-#articles_spread <- articles_text %>%
+#articles_spread <- df_text_codes %>%
 #  group_by(Title.x) %>%
 #  separate(Focus)
 
 # Drop duplicated 
-#articles_text <- articles_text[!duplicated(articles_text$Title.x), ]
+#df_text_codes <- df_text_codes[!duplicated(df_text_codes$Title.x), ]
 
 ## Clean up contractions
-articles_text$Article_Text <- str_replace(articles_text$Article_Text, "we're", "we are")
-articles_text$Article_Text <- str_replace(articles_text$Article_Text, "i'm", "i am")
-articles_text$Article_Text <- str_replace(articles_text$Article_Text, "don't", "do not")
-articles_text$Article_Text <- str_replace(articles_text$Article_Text, "it's", "it is")
+df_text_codes$Article_Text <- str_replace(df_text_codes$Article_Text, "we're", "we are")
+df_text_codes$Article_Text <- str_replace(df_text_codes$Article_Text, "i'm", "i am")
+df_text_codes$Article_Text <- str_replace(df_text_codes$Article_Text, "don't", "do not")
+df_text_codes$Article_Text <- str_replace(df_text_codes$Article_Text, "it's", "it is")
 
-articles_text <- articles_text %>%
+df_text_codes <- df_text_codes %>%
   select(Title.x, Publication_State, Focus, Conflict_Type, Value_Orientation, Article_Text)
 
 # save cleaned up data
-write_csv(articles_text, here::here(paste0("data/processed/grizz_clean_text_", Sys.Date(), ".csv")), 
+write_csv(df_text_codes, here::here(paste0("data/processed/clean_text_", Sys.Date(), ".csv")), 
           col_names = TRUE)
 
 ## Tokenize
