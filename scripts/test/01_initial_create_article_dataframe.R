8a941c437acd73fbfa133c77e69036ae6b7b915b
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-06-07 15:14:46 -0600

    tested out using rvest to read htmls. rvest provides cleaner html parsing than my previous code

--- scripts/test/01_initial_create_article_dataframe.R
+++ scripts/test/01_initial_create_article_dataframe.R
@@ -10,7 +10,7 @@ options(
 
 # Load the new_article_coding_ids.csv
 article_codes <- read.csv(file = "data/original/new_article_coding.csv")
-article_list_prev <- read.csv(file = "data/processed/article_list_20-02-2024-1405.csv") 
+article_list_prev <- read.csv(file = "data/processed/article_list_07-06-2024-1430.csv") 
 article_list_prev <- article_list_prev %>% 
   select(X, Link, ID)
 
@@ -37,48 +37,107 @@ boars <- article_codes %>%
 other <- article_codes %>%
   filter(Species == "Other")
 
-urls <- trimws(unique(other$Link))
+urls <- trimws(unique(grizz$Link))
 
 # Set up a user agent so that the website doesn't think you are a robot
 ua <- "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
 
 # Create an empty date. We will append the new article to this data frame
-df_article <- data.frame(Title=character(),
-                         Author=character(),
-                         Published_Date=character(),
-                         Article_Text=character(), 
-                         Link=character())
+df_article <- data.frame(Title = character(),
+                         Author = character(),
+                         Published_Date = character(),
+                         Source = character(),
+                         Article_Text = character(), 
+                         Link = character())
 
 #df_article <- read_csv("data/processed/article_text.csv")
 
-for (url in urls[66:135]){
+for (url in urls[1:2]){
   link <- url
   page <- GET(link, user_agent(ua))
-  page_text <- httr::content(page, as = "text")
-  tmp <- gsub(".*<h1", "", page_text) 
-  tmp_noline <- str_remove_all(tmp, "\n")
-  article_title <- str_extract_all(tmp_noline, regex("(?<=clipcopy\">).+?(?=</h1>)"))
+  page_text <- read_html(page) 
+  article_title <- page_text %>% html_element("h1") %>% html_text2()
   if(lengths(article_title) == 0){
     article_title <- "none given"
   } else{article_title <- article_title}
-  author <- str_extract_all(tmp_noline, regex("(?<=Byline: ).+?(?=</span>)"))
+  author <- page_text %>% html_nodes("span.author-byline") %>% html_text2()
   if(lengths(author) == 0){
     author <- "none given"
   } else{author <- author}
-  pub_date <- str_extract_all(tmp_noline, regex("(?<=display-date\">).+?(?=</span>)"))
+  pub_date <- page_text %>% html_nodes("span.display-date") %>% html_text2()
   if(lengths(pub_date) == 0){
     pub_date <- "none given"
   } else{pub_date <- pub_date}
-  article_text <- str_extract(tmp_noline, regex("(?<=body--ascii \">).+?(?=</p>)"))
+  source <- page_text %>% html_nodes("span.source") %>% html_text2()
+  if(lengths(source) == 0){
+    source <- "none given"
+  } else{source <- source}
+  article_text_all <- page_text %>% html_elements("p") %>% html_text2()
+  article_text <- article_text_all[9]
   if(lengths(article_text) == 0){
     article_text <- "pdf of actual newspaper"
   } else{article_text <- article_text}
-  df_article[nrow(df_article) + 1,] <- c(article_title, 
-                                         author, 
-                                         pub_date, 
-                                         article_text,
-                                         link)
+  df_article_test[nrow(df_article_test) + 1,] <- c(article_title, 
+                                                   author, 
+                                                   pub_date, 
+                                                   source,
+                                                   article_text,
+                                                   link)
 }
+
+## testing using read_html() from rvest
+page2 <- GET(urls[2], user_agent(ua))
+page2_test <- read_html(page2)
+author_text2 <- page2_test %>% html_nodes("span.author-byline") %>% html_text2()
+date_text2 <- page2_test %>% html_nodes("span.display-date") %>% html_text2()
+source_text2 <- page2_test %>% html_nodes("span.source") %>% html_text2()
+text2_test <- page2_test %>% html_elements("p") %>% html_text2()
+article_text2 <- text2_test[9]
+title_text2 <- page2_test %>% html_element("h1") %>% html_text2()
+
+## test new for-loop
+### create new data frame that includes source
+df_article_test <- data.frame(Title = character(),
+                              Author = character(),
+                              Published_Date = character(),
+                              Source = character(),
+                              Article_Text = character(), 
+                              Link = character())
+
+for (url in urls[1:2]){
+  link <- url
+  page <- GET(link, user_agent(ua))
+  page_text <- read_html(page) 
+  article_title <- page_text %>% html_element("h1") %>% html_text2()
+  if(lengths(article_title) == 0){
+    article_title <- "none given"
+  } else{article_title <- article_title}
+  author <- page_text %>% html_nodes("span.author-byline") %>% html_text2()
+  if(lengths(author) == 0){
+    author <- "none given"
+  } else{author <- author}
+  pub_date <- page_text %>% html_nodes("span.display-date") %>% html_text2()
+  if(lengths(pub_date) == 0){
+    pub_date <- "none given"
+  } else{pub_date <- pub_date}
+  source <- page_text %>% html_nodes("span.source") %>% html_text2()
+  if(lengths(source) == 0){
+    source <- "none given"
+  } else{source <- source}
+  article_text_all <- page_text %>% html_elements("p") %>% html_text2()
+  article_text <- article_text_all[9]
+  if(lengths(article_text) == 0){
+    article_text <- "pdf of actual newspaper"
+  } else{article_text <- article_text}
+  df_article_test[nrow(df_article_test) + 1,] <- c(article_title, 
+                                                   author, 
+                                                   pub_date, 
+                                                   source,
+                                                   article_text,
+                                                   link)
+}
+
+
 
 ## Not sure if this is necessary or if I can just join by the link
 #df_article$ID <- df_article %>% 
