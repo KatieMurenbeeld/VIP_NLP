9cbb04f812cc42de466808a9b259bbb55498fecd
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-06-07 15:29:26 -0600

    changed if else statements in for loop length() instead of lengths()

--- scripts/test/01_initial_create_article_dataframe.R
+++ scripts/test/01_initial_create_article_dataframe.R
@@ -37,6 +37,7 @@ boars <- article_codes %>%
 other <- article_codes %>%
   filter(Species == "Other")
 
+species <- "grizzly_bears"
 urls <- trimws(unique(grizz$Link))
 
 # Set up a user agent so that the website doesn't think you are a robot
@@ -52,102 +53,42 @@ df_article <- data.frame(Title = character(),
 
 #df_article <- read_csv("data/processed/article_text.csv")
 
-for (url in urls[1:2]){
+for (url in urls[5:10]){
   link <- url
   page <- GET(link, user_agent(ua))
   page_text <- read_html(page) 
   article_title <- page_text %>% html_element("h1") %>% html_text2()
-  if(lengths(article_title) == 0){
+  if(length(article_title) == 0){
     article_title <- "none given"
   } else{article_title <- article_title}
   author <- page_text %>% html_nodes("span.author-byline") %>% html_text2()
-  if(lengths(author) == 0){
+  if(length(author) == 0){
     author <- "none given"
   } else{author <- author}
   pub_date <- page_text %>% html_nodes("span.display-date") %>% html_text2()
-  if(lengths(pub_date) == 0){
+  if(length(pub_date) == 0){
     pub_date <- "none given"
   } else{pub_date <- pub_date}
   source <- page_text %>% html_nodes("span.source") %>% html_text2()
-  if(lengths(source) == 0){
+  if(length(source) == 0){
     source <- "none given"
   } else{source <- source}
   article_text_all <- page_text %>% html_elements("p") %>% html_text2()
   article_text <- article_text_all[9]
-  if(lengths(article_text) == 0){
+  if(length(article_text) == 0){
     article_text <- "pdf of actual newspaper"
   } else{article_text <- article_text}
-  df_article_test[nrow(df_article_test) + 1,] <- c(article_title, 
-                                                   author, 
-                                                   pub_date, 
-                                                   source,
-                                                   article_text,
-                                                   link)
+  df_article[nrow(df_article) + 1,] <- c(article_title, 
+                                         author, 
+                                         pub_date, 
+                                         source,
+                                         article_text,
+                                         link)
 }
 
-## testing using read_html() from rvest
-page2 <- GET(urls[2], user_agent(ua))
-page2_test <- read_html(page2)
-author_text2 <- page2_test %>% html_nodes("span.author-byline") %>% html_text2()
-date_text2 <- page2_test %>% html_nodes("span.display-date") %>% html_text2()
-source_text2 <- page2_test %>% html_nodes("span.source") %>% html_text2()
-text2_test <- page2_test %>% html_elements("p") %>% html_text2()
-article_text2 <- text2_test[9]
-title_text2 <- page2_test %>% html_element("h1") %>% html_text2()
-
-## test new for-loop
-### create new data frame that includes source
-df_article_test <- data.frame(Title = character(),
-                              Author = character(),
-                              Published_Date = character(),
-                              Source = character(),
-                              Article_Text = character(), 
-                              Link = character())
-
-for (url in urls[1:2]){
-  link <- url
-  page <- GET(link, user_agent(ua))
-  page_text <- read_html(page) 
-  article_title <- page_text %>% html_element("h1") %>% html_text2()
-  if(lengths(article_title) == 0){
-    article_title <- "none given"
-  } else{article_title <- article_title}
-  author <- page_text %>% html_nodes("span.author-byline") %>% html_text2()
-  if(lengths(author) == 0){
-    author <- "none given"
-  } else{author <- author}
-  pub_date <- page_text %>% html_nodes("span.display-date") %>% html_text2()
-  if(lengths(pub_date) == 0){
-    pub_date <- "none given"
-  } else{pub_date <- pub_date}
-  source <- page_text %>% html_nodes("span.source") %>% html_text2()
-  if(lengths(source) == 0){
-    source <- "none given"
-  } else{source <- source}
-  article_text_all <- page_text %>% html_elements("p") %>% html_text2()
-  article_text <- article_text_all[9]
-  if(lengths(article_text) == 0){
-    article_text <- "pdf of actual newspaper"
-  } else{article_text <- article_text}
-  df_article_test[nrow(df_article_test) + 1,] <- c(article_title, 
-                                                   author, 
-                                                   pub_date, 
-                                                   source,
-                                                   article_text,
-                                                   link)
-}
-
-
-
-## Not sure if this is necessary or if I can just join by the link
-#df_article$ID <- df_article %>% 
-#  filter(!Link=='') %>%
-#  mutate(tmp = match(Link, unique(Link)),
-#         ID = sprintf('Article%003d', tmp)) %>%
-#  select(-tmp)
 
 ## Write df to csv
-write_csv(df_article, here::here(paste0("data/processed/article_text_coyote_", Sys.Date(), ".csv")), 
+write_csv(df_article, here::here(paste0("data/processed/article_text_", species, Sys.Date(), ".csv")), 
                                         col_names = TRUE, 
                                         append = TRUE)
 
@@ -162,9 +103,6 @@ write_csv(df_text_codes, here::here(paste0("data/processed/article_text_codes_co
                                            Sys.Date(), ".csv")), 
                                            col_names = TRUE, 
                                            append = TRUE)
-
-#################
-# Breadcrumb: Need to figure out if else for articles with no by lines (if character(0) then NA else text)
 
 
 
