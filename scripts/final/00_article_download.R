c2281df64ab70febe79e3389af135fc85160639e
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-09-10 16:39:20 -0600

    updated google sheet

--- scripts/final/00_article_download.R
+++ scripts/final/00_article_download.R
@@ -15,10 +15,10 @@ folder_url <- "https://drive.google.com/drive/folders/1ob5sagTtT3svhc7ZKeemd9TiA
 folder <- drive_get(as_id(folder_url))
 
 gdrive_files <- drive_ls(folder)
-id <- gdrive_files[gdrive_files$name == "New Article Coding Framework", ]$id
-drive_download(id, path = "data/original/new_article_coding.csv", overwrite = TRUE)
+id <- gdrive_files[gdrive_files$name == "James Edited Values", ]$id
+drive_download(id, path = "data/original/james_edited_values_coding.csv", overwrite = TRUE)
 
-article_codes <- read.csv(file = "data/original/new_article_coding.csv")
+article_codes <- read.csv(file = "data/original/james_edited_values_coding.csv")
 
 # Create a unique article ID
 article_codes <- article_codes %>% 
@@ -27,7 +27,7 @@ article_codes <- article_codes %>%
          ID = sprintf('Article%003d', tmp)) %>%
   select(-tmp)
 # Save as a new csv in the data/processed folder
-#write.csv(article_codes, "data/processed/new_article_coding_ids.csv")
+write.csv(article_codes, here::here(paste0("data/processed/new_article_coding_ids_", Sys.Date(), ".csv")))
 
 # Save a csv to reference newly added articles
 article_list <- article_codes %>%
