1687c21de2e0cbe17c8995e2d525227cdb21480f
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-12-04 16:05:27 -0700

    added library and code to test tuning and training knn model

--- scripts/test/_tidymodels_testing.R
+++ scripts/test/_tidymodels_testing.R
@@ -12,6 +12,7 @@ library(utiml)
 library(tm)
 library(caret)
 library(kableExtra)
+library(kknn)
 
 
 # Load the cleaned data
@@ -51,7 +52,6 @@ nb_spec <- naive_Bayes() %>%
 
 nb_spec
 
-
 nb_fit <- text_wf %>%
   add_model(nb_spec) %>%
   fit(data = text_train)
@@ -86,7 +86,59 @@ final_wf <- final_fit %>%
 predict(final_wf, new_data = unlabeled_articles) #it worked!!
 
 # this workflow worked. now I need to train and test knn, svm, and rf
+# Tuning and training knn from 
+# https://optimumsportsperformance.com/blog/k-nearest-neighbor-tidymodels-tutorial/
 
+knn_spec <- nearest_neighbor(neighbors = tune()) %>%
+  set_engine("kknn") %>%
+  set_mode("classification")
 
+knn_wf <- workflow() %>%
+  add_recipe(text_rec) %>%
+  add_model(knn_spec)
 
+cntrl <- control_resamples(save_pred = TRUE)
 
+knn_tune_grid <- grid_regular(
+  neighbors(range = c(2, 10))
+)
+
+knn_tune <- tune_grid(
+  knn_wf,
+  resamples = text_folds,
+  grid = knn_tune_grid, 
+  control = cntrl
+)
+  
+knn_tune %>% 
+  collect_metrics()
+  
+best_neighbors <- knn_tune %>%
+  select_best(metric = "accuracy") %>%
+  pull(neighbors)
+
+knn_final <- knn_wf %>%
+  finalize_workflow(
+    select_best(x = knn_tune, metric = "accuracy")
+  )
+
+knn_rs <- fit_resamples(
+  knn_final, 
+  text_folds, 
+  control = control_resamples(save_pred = TRUE)
+)
+
+knn_rs_metrics <- collect_metrics(knn_rs)
+knn_rs_predictions <- collect_predictions(knn_rs)
+
+knn_rs_predictions %>%
+  filter(id == "Fold10") %>%
+  conf_mat(truth = Value_Orientation, .pred_class) %>%
+  autoplot(type = "heatmap") 
+
+knn_final_fit <- last_fit(knn_final, text_split)
+
+knn_final_wf <- knn_final_fit %>%
+  extract_workflow()
+
+predict(knn_final_wf, new_data = unlabeled_articles) #it worked!!
