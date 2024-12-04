05f91552dfa8b0caad3f939a3317f5b6cef03076
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-12-04 16:05:59 -0700

    separate script to train, tune, and fit knn classification with tidymodels

---
+++ scripts/test/_tidymodels_knn_testing.R
@@ -0,0 +1,141 @@
+library(tidyverse)
+library(tidymodels)
+library(tidytext)
+library(textrecipes)
+library(discrim)
+library(themis)
+library(naivebayes)
+library(recipes)
+library(hardhat)
+library(glmnet)
+library(utiml)
+library(tm)
+library(caret)
+library(kableExtra)
+library(kknn)
+
+
+# Load the cleaned data
+articles_text_clean <- read_csv(here::here("data/processed/clean_text_2024-09-16.csv"))
+
+# drop any rows with NA for Value_Orientation
+articles_text_clean <- articles_text_clean[!is.na(articles_text_clean$Value_Orientation), ]
+
+# Load the unlabeled documents
+unlabeled_articles <- read_csv(here::here("data/processed/newdata_for_model_testing_text_2024-11-06.csv"))
+unlabeled_articles <- unlabeled_articles %>%
+  dplyr::select(Article_Text)
+
+
+text2wvo <- articles_text_clean %>%
+  dplyr::select(Article_Text, Value_Orientation)
+
+text2wvo$Value_Orientation <- as.factor(text2wvo$Value_Orientation)
+
+text_split <- initial_split(text2wvo, strat = Value_Orientation)
+
+text_train <- training(text_split)
+text_test <- testing(text_split)
+
+
+text_rec <- 
+  recipe(Value_Orientation ~ Article_Text, data = text_train)
+
+text_rec_v1 <- text_rec %>%
+  step_tokenize(Article_Text) %>%
+  step_tokenfilter(Article_Text, max_tokens = 1000) %>%
+  step_tfidf(Article_Text)
+  #step_downsample(Value_Orientation) #downscale for unbalanced data
+
+text_rec_v2 <- text_rec %>%
+  step_tokenize(Article_Text) %>%
+  step_tokenfilter(Article_Text, 
+                   max_tokens = tune(),
+                   min_times = 100) %>%
+  step_tfidf(Article_Text)
+
+set.seed(234)
+text_folds <- vfold_cv(text_train)
+
+text_wf_v1 <- workflow() %>%
+  add_recipe(text_rec_v1)
+
+text_wf_v2 <- workflow() %>%
+  add_recipe(text_rec_v2)
+
+# parameter tuning 
+cntrl <- control_resamples(save_pred = TRUE)
+
+knn_tune_grid <- grid_regular(
+  neighbors(range = c(2, 10))
+)
+
+final_tune_grid <- grid_regular(
+  neighbors(range = c(2, 10)), 
+  max_tokens(range = c(1000, 3000)),
+  levels = c(neighbors = 9, max_tokens = 3)
+)
+final_tune_grid
+
+## tuneable knn model
+knn_spec <- nearest_neighbor(neighbors = tune()) %>%
+  set_engine("kknn") %>%
+  set_mode("classification")
+
+## create workflow with tuneable model
+knn_tune_wf <- workflow() %>%
+  add_recipe(text_rec_v2) %>%
+  add_model(knn_spec)
+
+set.seed(6891)
+knn_tune_rs <- tune_grid(
+  knn_tune_wf,
+  resamples = text_folds,
+  grid = final_tune_grid, 
+  control = cntrl
+)
+
+#collect_metrics(knn_tune_rs)
+autoplot(knn_tune_rs)
+
+knn_tune_rs %>%
+  collect_predictions() %>%
+  filter(id == "Fold02") %>%
+  conf_mat(truth = Value_Orientation, .pred_class) %>%
+  autoplot(type = "heatmap") 
+
+
+best_neighbors <- knn_tune %>%
+  select_best(metric = "accuracy") %>%
+  pull(neighbors)
+
+knn_final <- knn_tune_wf %>%
+  finalize_workflow(
+    select_best(x = knn_tune_rs, metric = "accuracy")
+  )
+
+knn_rs <- fit_resamples(
+  knn_final, 
+  text_folds, 
+  control = control_resamples(save_pred = TRUE)
+)
+
+knn_rs_metrics <- collect_metrics(knn_rs)
+knn_rs_predictions <- collect_predictions(knn_rs)
+
+knn_rs_predictions %>%
+  filter(id == "Fold02") %>%
+  conf_mat(truth = Value_Orientation, .pred_class) %>%
+  autoplot(type = "heatmap") 
+
+# try out the lasso regression?
+
+lasso_wf <- workflow() %>%
+  add_recipe(text_rec)
+
+
+
+
+
+
+
