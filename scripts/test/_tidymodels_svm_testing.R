9472d179f26bf3aa86d8a4eeaac5ae4583facf9c
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-12-09 16:11:50 -0700

    updated tuning grid and tuning workflow for svm model

--- scripts/test/_tidymodels_svm_testing.R
+++ scripts/test/_tidymodels_svm_testing.R
@@ -41,17 +41,27 @@ text_test <- testing(text_split)
 text_rec <- 
   recipe(Value_Orientation ~ Article_Text, data = text_train)
 
-text_rec <- text_rec %>%
+text_rec_v1 <- text_rec %>%
   step_tokenize(Article_Text) %>%
   step_tokenfilter(Article_Text, max_tokens = 1000) %>%
   step_tfidf(Article_Text)
 #step_downsample(Value_Orientation) #downscale for unbalanced data
 
-set.seed(2345)
+text_rec_v2 <- text_rec %>%
+  step_tokenize(Article_Text) %>%
+  step_tokenfilter(Article_Text, 
+                   max_tokens = tune(),
+                   min_times = 100) %>%
+  step_tfidf(Article_Text)
+
+set.seed(234)
 text_folds <- vfold_cv(text_train)
 
-text_wf <- workflow() %>%
-  add_recipe(text_rec)
+text_wf_v1 <- workflow() %>%
+  add_recipe(text_rec_v1)
+
+text_wf_v2 <- workflow() %>%
+  add_recipe(text_rec_v2)
 
 # parameter tuning 
 cntrl <- control_resamples(save_pred = TRUE)
@@ -59,9 +69,28 @@ svm_tune_grid <- grid_regular(
   cost(range = c(-10,5))
 )
 
-svm_spec <- svm_rbf(cost = tune()) %>%
-  set_engine("kernlab") %>%
+final_tune_grid <- grid_regular(
+  cost(range = c(-10,5)), 
+  #max_tokens(range = c(1000, 3000)),
+  max_tokens(range = c(500, 1300)),
+  levels = c(cost = 16, max_tokens = 9)
+)
+final_tune_grid
+
+svm_spec <- svm_linear(cost = tune()) %>%
+  set_engine("LiblineaR") %>%
   set_mode("classification")
 
-svm_spec
+## create workflow with tuneable model
+svm_tune_wf <- workflow() %>%
+  add_recipe(text_rec_v2) %>%
+  add_model(svm_spec)
+
+set.seed(6891)
+svm_tune_rs <- tune_grid(
+  svm_tune_wf,
+  resamples = text_folds,
+  grid = final_tune_grid, 
+  control = cntrl
+)
 
