624fe1ff8cddda06937dcf4245042c24d67edcc7
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-12-12 15:10:30 -0700

    updated parameters to tune

--- scripts/test/_tidymodels_rf_testing.R
+++ scripts/test/_tidymodels_rf_testing.R
@@ -70,10 +70,14 @@ rf_tune_grid <- grid_regular(
 )
 
 final_tune_grid <- grid_regular(
-  cost(range = c(-10,5)), 
-  #max_tokens(range = c(1000, 3000)),
+  trees(range = c(1000, 5000)), 
+  mtry(range = c(10, 30)),
+  min_n(range = c(2, 8)),
   max_tokens(range = c(500, 1300)),
-  levels = c(cost = 16, max_tokens = 9)
+  levels = c(trees = 5, 
+             mtry = 5, 
+             min_n = 5, 
+             max_tokens = 9)
 )
 final_tune_grid
 
@@ -98,3 +102,38 @@ rf_tune_rs <- tune_grid(
   control = cntrl
 )
 
+autoplot(rf_tune_rs)
+
+rf_tune_rs %>%
+  collect_predictions() %>%
+  filter(id == "Fold02") %>%
+  conf_mat(truth = Value_Orientation, .pred_class) %>%
+  autoplot(type = "heatmap") 
+
+best_trees <- rf_tune_rs %>%
+  select_best(metric = "accuracy") %>%
+  pull(neighbors)
+
+#somewhere need to update the text_rec with the best max tokens
+# max_tokens = 500 and neighbors = 5
+
+rf_final <- rf_tune_wf %>%
+  finalize_workflow(
+    select_best(x = rf_tune_rs, metric = "accuracy")
+  )
+
+rf_final
+
+rf_rs <- fit_resamples(
+  rf_final, 
+  text_folds, 
+  control = control_resamples(save_pred = TRUE)
+)
+
+rf_rs_metrics <- collect_metrics(rf_rs)
+rf_rs_predictions <- collect_predictions(rf_rs)
+
+rf_rs_predictions %>%
+  filter(id == "Fold02") %>%
+  conf_mat(truth = Value_Orientation, .pred_class) %>%
+  autoplot(type = "heatmap") 
