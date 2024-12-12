b780ccdb05ccc454a50416ad1596ef8086d94fbd
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-12-12 15:10:13 -0700

    tested prediction with new articles

--- scripts/test/_tidymodels_knn_testing.R
+++ scripts/test/_tidymodels_knn_testing.R
@@ -26,6 +26,11 @@ unlabeled_articles <- read_csv(here::here("data/processed/newdata_for_model_test
 unlabeled_articles <- unlabeled_articles %>%
   dplyr::select(Article_Text)
 
+tdm_articles <- read_csv(here::here("data/original/grizzly_bear_corpus_gamma_threshold_0.5.csv"))
+tdm_articles <- tdm_articles %>%
+  dplyr::select(Text) %>%
+  rename(Article_Text = Text)
+
 
 text2wvo <- articles_text_clean %>%
   dplyr::select(Article_Text, Value_Orientation)
@@ -43,9 +48,9 @@ text_rec <-
 
 text_rec_v1 <- text_rec %>%
   step_tokenize(Article_Text) %>%
-  step_tokenfilter(Article_Text, max_tokens = 1000) %>%
-  step_tfidf(Article_Text)
-  #step_downsample(Value_Orientation) #downscale for unbalanced data
+  step_tokenfilter(Article_Text, max_tokens = tune(), min_times = 100) %>%
+  step_tfidf(Article_Text) %>%
+  step_downsample(Value_Orientation) #downscale for unbalanced data
 
 text_rec_v2 <- text_rec %>%
   step_tokenize(Article_Text) %>%
@@ -85,10 +90,10 @@ knn_spec <- nearest_neighbor(neighbors = tune()) %>%
 
 ## create workflow with tuneable model
 knn_tune_wf <- workflow() %>%
-  add_recipe(text_rec_v2) %>%
+  add_recipe(text_rec_v1) %>%
   add_model(knn_spec)
 
-set.seed(6891)
+set.seed(2244)
 knn_tune_rs <- tune_grid(
   knn_tune_wf,
   resamples = text_folds,
@@ -96,7 +101,6 @@ knn_tune_rs <- tune_grid(
   control = cntrl
 )
 
-#collect_metrics(knn_tune_rs)
 autoplot(knn_tune_rs)
 
 knn_tune_rs %>%
@@ -138,7 +142,44 @@ knn_rs_predictions %>%
   conf_mat(truth = Value_Orientation, .pred_class) %>%
   autoplot(type = "heatmap") 
 
-# try out the lasso regression?
+
+knn_final_fit <- last_fit(knn_final, text_split)
+
+knn_final_wf <- knn_final_fit %>%
+  extract_workflow()
+
+test_final_wf <- knn_final_fit %>%
+  extract_fit_parsnip()
+
+predict(knn_final_wf, new_data = tdm_articles) #it worked!! with both my test "unlabeled_articles" and the "tdm_articles"
+
+# So I at least know that I can use this code with the csvs that Lily made on TDM
+write_rds(knn_final_wf, file = here::here("output/models/test_final_wf.rds"))
+test_wf <- readRDS(here::here("output/models/test_final_wf.rds"))
+test_wf
+predict(test_wf, new_data = unlabeled_articles)
+# I should be able to save as an rds, load to tdm and then use it there. 
+
+## Add a document id column
+tdm_articles$id <- seq.int(nrow(tdm_articles))
+
+tidy_text_unlabel <- tdm_articles %>%
+  unnest_tokens(word, Article_Text) %>% 
+  filter(!grepl('[0-9]', word)) 
+
+dtm_test <- tidy_text_unlabel %>%
+  count(id, word, sort = TRUE) %>%
+  bind_tf_idf(word, id, n) %>%
+  cast_dtm(id, word, tf_idf)
+
+dtm_test_df <- dtm_test %>% as.matrix() %>% as.data.frame() 
+predict(knn_final_wf, new_data = dtm_test_df) # i can't use a premade dtm. Tidymodels wants to go through the entire process. 
+
+# maybe if I make a new workflow? But I might need to use the same work flow to train the model...
+
+
+
+# try out the lasso regression? Is that what I'm doing with the tune grids?
 
 lasso_wf <- workflow() %>%
   add_recipe(text_rec)
