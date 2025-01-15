5012681eaa74a783248b0b4d23926e3542aa0f9a
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-01-15 16:23:12 -0700

    script to create a subsample of articles for James to hand code and compare to predicted results

---
+++ scripts/test/_grizzly_preds_rsamp_for_validation.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+#---Load the data----
+## use this because it has the most articles
+gbear_05_preds <- read_csv(here::here("output/predictions/grizzly_bear_05_preds_gamma.csv"))
+
+
+print(nrow(gbear_05_preds) * 0.05)
+
+#get a sample of 10
+set.seed(2486)
+samp_5pct <- sample(nrow(gbear_05_preds),273)
+
+#data in the sample
+samp_5pct_df <- gbear_05_preds[samp_5pct,]
+
+
+hist(samp_5pct_df$reg_05_pred_class)
+hist(samp_5pct_df$knn_05_pred_class)
+hist(samp_5pct_df$rf_05_pred_class)
